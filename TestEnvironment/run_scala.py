fc409778e566d4d64b6952c860035052a3cb33c1
Andreas Prinz <andreas.prinz@uia.no>
2024-08-27 15:38:36 +0200

    again adapt to 3.5

--- TestEnvironment/run_scala.py
+++ TestEnvironment/run_scala.py
@@ -1,6 +1,5 @@
 def run_commands(inf, outf):
     commands = []
-    commands.append(['scala', ['solver.jar']])
-#    commands.append(['scala', ['solver.jar', inf, outf]])
+    commands.append(['scala', ['solver.jar', '--', inf, outf]])
 #    commands.append(['ls', ['-lisa']])
     return commands
